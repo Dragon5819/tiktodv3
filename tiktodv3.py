a13f7b7ae505322d8678c6714cdcd3a255f16423
Dragon5819 <113884385+Dragon5819@users.noreply.github.com>
2022-10-11 15:43:30 +0700

    Update tiktodv3.py

--- tiktodv3.py
+++ tiktodv3.py
@@ -1,9 +1,7 @@
 from selenium import webdriver
 from os import system, name
-import chromedriver_binary
 from time import time, strftime, gmtime, sleep
 import pyfiglet, os, threading
-import chromedriver_autoinstaller
 
 # Check if the current version of chromedriver exists
 # and if it doesn't exist, download it automatically,
